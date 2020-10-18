8fc3de397db65342baee66e6957f7a07fa9201ac
labib yanes <labibyanes2@gmail.com>
2020-10-18 22:13:27 +0200

    props-chkpt

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Profile from './profile/profile';
+import Picture from './profile/luca-modric.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile  fullname = "Luca Modric" bio = "né le 9 septembre 1985 à Zadar en Yougoslavie (aujourd'hui en Croatie), est un footballeur international croate. Il évolue au poste de milieu de terrain au Real Madrid CF. Il est, avec Davor Šuker et Bernard Vukas, considéré comme l'un des plus grands joueurs croates de l'histoire Il est aussi le seul joueur croate à avoir gagné le ballon d'or" profession="Joueur du football " >
+        <img
+        className="imgPro"
+        src={Picture}
+        alt="luca modric"
+        height={"500px"}
+        width={"800px"}
+        
+        
+        />
+      </Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile/profile.js
@@ -0,0 +1,47 @@
+import React from 'react'; 
+import PropTypes from "prop-types";
+
+const Profile = props=>{
+ function handleName (a) {
+     a.preventDefault();
+    alert("Luca Modric");
+
+ }
+
+
+    return (
+
+        <div className="container">
+            <div onClick={handleName}>
+                {props.children}
+                </div>
+            <div className="left-row">
+                <h1  style={{ fontSize: "50px", color:" rgb(109 95 24)" }}  >{props.fullname}</h1>
+                <h2   style={{ fontSize: "30px", color:"#635f5f" }} >{props.profession}</h2>
+                <p   style={{ fontSize: "14px", color:"white" , textAlign: "justify" }}>{props.bio}</p>
+        </div>
+
+
+
+        
+        </div>
+
+
+
+    );
+}
+export default Profile;
+
+Profile.defaultProps={
+fullname:"luca modric ",
+proffession:"Joueur du football",
+bio:"joueur de l'equipe real madrid"
+
+};
+
+Profile.propType = {
+    fullname: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    profession: PropTypes.string.isRequired
+  };
+
